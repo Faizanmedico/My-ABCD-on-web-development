d6c88648e17e0c1c75b125cfb567b9627c5b492a
Sultan Ahmed <146020547+Faizanmedico@users.noreply.github.com>
2024-01-25 12:38:43 +0500

    Add files via upload
    My ABCD on web development....Link HTML, CSS, JAVASCRIPT.

---
+++ myScript.js
@@ -0,0 +1,16 @@
+document.write("Sultan on javascript");
+document.getElementById("demo").innerHTML = "My First JavaScript SULTAN AHMED";
+
+
+//s2
+function myFunction() {
+  document.getElementById("demo1").innerHTML = "Paragraph changed 88888888.";
+}
+
+
+//s3
+x = 5;
+y = 6;
+z = x + y;
+document.getElementById("demo2").innerHTML =
+"The value of z is: " + z;
